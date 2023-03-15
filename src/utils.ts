c9a3ce688e461d809ed5dec55fb0a2d39f3f7b65
unknown <github@adamhwang.com>
2023-03-15 01:16:35 -0500

    Fixed range bug

--- src/utils.ts
+++ src/utils.ts
@@ -4,8 +4,8 @@ export const formatUSD = (amount: number, places = 2) =>
         maximumFractionDigits: places,
     });
 
-export const rangeMinSize = (size: number, startAt: number, minSize = 10) => size >= minSize ? range(size, startAt) : range(minSize, startAt, size / minSize);
+export const rangeMinSize = (size: number, startAt: number, minSize = 10) => size >= minSize ? range(size + 1, startAt) : range(minSize + 1, startAt, size / minSize);
 
-export const range = (size: number, startAt = 0, step = 1): ReadonlyArray<number> => [...Array(size).keys()].map(i => (i + startAt) * step);
+export const range = (size: number, startAt = 0, step = 1): ReadonlyArray<number> => [...Array(size).keys()].map(i => startAt + (i * step));
 
 export const format = (input: string, ...args: string[]) => input.replace(/{(\d+)}/g, (match, number) => typeof args[number] != 'undefined' ? args[number] : match);
